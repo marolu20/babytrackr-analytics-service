236706574e064af8e4b3ae6ddd06c0425c9c8a2e
marolu20 <mariamolupitan@gmail.com>
2026-06-26 18:39:51 -0400

    remove delete functions from depositories

--- src/main/kotlin/com/babytrackr/analytics/infrastructure/repositories/DiaperEventRepository.kt
+++ src/main/kotlin/com/babytrackr/analytics/infrastructure/repositories/DiaperEventRepository.kt
@@ -2,4 +2,6 @@ package com.babytrackr.analytics.infrastructure.repositories
 
 import org.springframework.data.repository.CrudRepository
 
-interface DiaperEventRepository: CrudRepository<DiaperEvent, Long> {}
+interface DiaperEventRepository: CrudRepository<DiaperEvent, Long> {
+    fun findByEventId(eventId: Long): DiaperEvent?
+}
--- src/main/kotlin/com/babytrackr/analytics/infrastructure/repositories/SleepEventRepository.kt
+++ src/main/kotlin/com/babytrackr/analytics/infrastructure/repositories/SleepEventRepository.kt
@@ -2,4 +2,6 @@ package com.babytrackr.analytics.infrastructure.repositories
 
 import org.springframework.data.repository.CrudRepository
 
-interface SleepEventRepository: CrudRepository<SleepEvent, Long> {}
+interface SleepEventRepository: CrudRepository<SleepEvent, Long> {
+    fun findByEventId(eventId: Long): SleepEvent?
+}
